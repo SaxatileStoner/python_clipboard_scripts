f2e01fca019ce52ad5710416d0995319d82e8aa2
SaxatileStoner <christopherstoner0326@gmail.com>
2022-04-05 12:25:08 -0700

    Changed file structure; WARNING, listeners are not working.

---
+++ python_clip_scripts/convert_clipboard_camelCase.py
@@ -0,0 +1,29 @@
+from utils import win32_tray_popup
+from re import sub
+import win32clipboard
+
+
+def camel_case(s):
+    s = sub(r"(_|-)+", " ", s).title().replace(" ", "")
+    return ''.join([s[0].lower(), s[1:]])
+
+
+def __main__():
+    title = "convert_clipboard_camelCase.py"
+    win32clipboard.OpenClipboard()
+    try:
+        string = str(win32clipboard.GetClipboardData())
+    except TypeError:
+        win32_tray_popup.balloon_tip(
+            title, "Last Clipboard Entry not of a string type")
+
+    string = camel_case(string)
+    win32clipboard.EmptyClipboard()
+    win32clipboard.SetClipboardText(f"{string}")
+    win32clipboard.CloseClipboard()
+    win32_tray_popup.balloon_tip(
+        title, f"Converted last clipboarded item to camelCase! {string}")
+
+
+if __name__ == "__main__":
+    __main__()
