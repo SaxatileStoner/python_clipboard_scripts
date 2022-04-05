5380f26da9d52f46f29b7e515bc36699fa58335f
SaxatileStoner <christopherstoner0326@gmail.com>
2022-04-05 13:26:21 -0700

    Changed hotkey def snake_case=shift+alt+ctrl+- && camel_case=shift+alt+ctrl+=

--- python_clip_scripts/listener.py
+++ python_clip_scripts/listener.py
@@ -4,15 +4,20 @@ from pynput import keyboard
 
 
 def execute_snake():
+    """converts last clipboarded string object into snakecase
+    """
     convert_clipboard_snakecase.__main__()
 
 
 def execute_camel():
+    """converts last clipboarded string object into camelcase
+    """
     convert_clipboard_camelCase.__main__()
 
 
+# Runs the listener repeatedly to keep checking
 while True:
     with keyboard.GlobalHotKeys({
-        '<alt>+<ctrl>+r': execute_snake,
-            '<alt>+<ctrl>+t': execute_camel}) as h:
+        '<shift>+<alt>+<ctrl>+-': execute_snake,
+            '<shift>+<alt>+<ctrl>+=': execute_camel}) as h:
         h.join()
