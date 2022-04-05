c3b86946d0062dfdceb232c347e173209b7d1fb3
SaxatileStoner <christopherstoner0326@gmail.com>
2022-04-05 13:02:49 -0700

    Simplified down to listener.py

---
+++ python_clip_scripts/listener.py
@@ -0,0 +1,17 @@
+import convert_clipboard_snakecase
+import convert_clipboard_camelCase
+from pynput import keyboard
+
+
+def execute_snake():
+    convert_clipboard_snakecase.__main__()
+
+
+def execute_camel():
+    convert_clipboard_camelCase.__main__()
+
+
+with keyboard.GlobalHotKeys({
+    '<alt>+<ctrl>+r': execute_snake,
+        '<alt>+<ctrl>+t': execute_camel}) as h:
+    h.join()
